9339ddd88a9fe37b32aa5acb363f2572ff316d8b
Bikash mandal <mandalbikash933@gmail.com>
2019-04-28 20:06:33 +0545

    navigation drawer

--- app/src/main/java/com/bookmyhostel/HomePage.java
+++ app/src/main/java/com/bookmyhostel/HomePage.java
@@ -1,20 +1,115 @@
 package com.bookmyhostel;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class HomePage extends AppCompatActivity {
-    private Toolbar toolbar;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    Toolbar toolbar;
+    private MaterialSearchView materialSearchView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.nav_drawer);
 
         toolbar = findViewById(R.id.toolbar_id);
         setSupportActionBar(toolbar);
 
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.open_dawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        materialSearchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_id);
+        materialSearchView.setMenuItem(menuItem);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.profile_id){
+            Toast.makeText(this, "Profile Is selected", Toast.LENGTH_SHORT).show();
+        }
+        else if (id==R.id.about_id){
+            Toast.makeText(this, "about Is Selected", Toast.LENGTH_SHORT).show();
+        }
+        else if (id==R.id.contact_id){
+            Toast.makeText(this, "Contact Is Selected", Toast.LENGTH_SHORT).show();
+        }
+        else if (id==R.id.logout_id){
+            Toast.makeText(this, "Log-out Is Seleted", Toast.LENGTH_SHORT).show();
+        }
+        else if (id==R.id.search_id){
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.profile_id1:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.hostel_id:
+                Toast.makeText(this, "Hostels", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bookmark_id:
+                Intent homeintent = new Intent(HomePage.this, Bookmark.class);
+                startActivity(homeintent);
+                break;
+            case R.id.chanegPassword_id:
+                Toast.makeText(this, "Change Password", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contactUs_Id:
+                Toast.makeText(this, "Contact Us", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.aboutUs_id:
+                Toast.makeText(this, "About Us", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout1_id:
+                Toast.makeText(this, "Log Out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
